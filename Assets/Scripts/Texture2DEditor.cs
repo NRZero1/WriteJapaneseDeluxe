6c7563dd9369a91accd7ed00f036c6c3ae6d3b78
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2021-10-21 18:37:45 +0700

    Add bounding box for feature extraction

--- Assets/Scripts/Texture2DEditor.cs
+++ Assets/Scripts/Texture2DEditor.cs
@@ -10,11 +10,13 @@ public class Texture2DEditor : MonoBehaviour
     Vector2 mousePos = new Vector2();
     RectTransform rectTransform;
     [SerializeField] Button clearButton;
+    [SerializeField] Button ExtractionButton;
     public Color drawColor = new Color(0, 0, 0, 0);
     int width = 0;
     int height = 0;
     public int penRadius = 10;
     public Vector2 lastMousePos;
+
     void Start()
     {
         var rawImage = GetComponent<RawImage>();
@@ -38,7 +40,7 @@ public class Texture2DEditor : MonoBehaviour
         mousePos.x = width - (width / 2 - mousePos.x);
         mousePos.y = Mathf.Abs((height / 2 - mousePos.y) - height);
 
-        print(mousePos.x + " " + mousePos.y);
+        //print(mousePos.x + " " + mousePos.y);
 
         if (Input.GetMouseButton(0)) {
             float quality = 20;
@@ -56,9 +58,9 @@ public class Texture2DEditor : MonoBehaviour
 
     public void clearCanvas()
     {
-        for (int i = 0; i <= height; i++)
+        for (int i = 0; i <= height - 1; i++)
         {
-            for (int j = 0; j <= width; j++)
+            for (int j = 0; j <= width - 1; j++)
             {
                 texture2D.SetPixel(j, i, new Color(255, 255, 255, 255));
             }
@@ -84,5 +86,76 @@ public class Texture2DEditor : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void FeatureExtraction()
+    {
+        int width = texture2D.width;
+        int height =texture2D.height;
+        float[,] grayscale_data = Grayscale(texture2D, width, height);
+        bool[,] biner_data = Biner(grayscale_data, width, height);
+        Rect bounding_box = MinimalBoundingBox(biner_data, width, height);
+
+        DrawRect(texture2D, bounding_box);
+        print(bounding_box);
+    }
+
+    public float[,] Grayscale(Texture2D texture, int width, int height) {
+        float[,] grayscale_data = new float[width, height];
+        for(int y=0; y<height; y++) {
+            for(int x=0; x<width; x++) {
+                grayscale_data[x,y] = texture.GetPixel(x, y).grayscale;
+            }
+        }
+        return grayscale_data;
+    }
+
+    public bool[,] Biner(float[,] grayscale_data, int width, int height) {
+        bool[,] binary_data = new bool[width, height];
+        for(int y=0; y<height; y++) {
+            for(int x=0; x<width; x++) {
+                binary_data[x,y] = (grayscale_data[x,y] < 0.5f);
+            }
+        }
+        return binary_data;
+    }
+    
+    public Rect MinimalBoundingBox(bool[,] binary_data, int width, int height) {
+        Vector2 min_point = new Vector2(width, height);
+        Vector2 max_point = new Vector2(0, 0);
+
+        for(int y=0; y<height; y++) {
+            for(int x=0; x<width; x++) {
+                if(binary_data[x,y]) {
+                    min_point.x = Mathf.Min(min_point.x, x);
+                    min_point.y = Mathf.Min(min_point.y, y);
+                    max_point.x = Mathf.Max(max_point.x, x);
+                    max_point.y = Mathf.Max(max_point.y, y);
+                }
+            }
+        }
+
+        return new Rect(min_point, max_point-min_point);
+    }
+
+    public void DrawRect(Texture2D texture2D, Rect rect) {
+        Vector2 p0 = new Vector2(rect.position.x            , rect.position.y);
+        Vector2 p1 = new Vector2(rect.position.x+rect.size.x, rect.position.y);
+        Vector2 p2 = new Vector2(rect.position.x            , rect.position.y+rect.size.y);
+        Vector2 p3 = new Vector2(rect.position.x+rect.size.x, rect.position.y+rect.size.y);
+
+        DrawLine(texture2D, p0, p1);
+        DrawLine(texture2D, p0, p2);
+        DrawLine(texture2D, p1, p3);
+        DrawLine(texture2D, p2, p3);
+    }
+
+    public void DrawLine(Texture2D texture2D, Vector2 start, Vector2 end) {
+        for(int y=(int)start.y; y<=end.y; y++) {
+            for(int x=(int)start.x; x<=end.x; x++) {
+                texture2D.SetPixel(x, y, new Color(0 ,0, 0, 255));
+            }
+        }
+        texture2D.Apply();
     }
 }
